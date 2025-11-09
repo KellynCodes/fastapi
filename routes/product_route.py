247f9e9a59ff4688e3813fa7bcb5c9058f64c28d
KellynCodes <kellyncodes@gmail.com>
2025-11-09 01:47:36 +0100

    ad method return types on product controller

--- routes/product_route.py
+++ routes/product_route.py
@@ -13,7 +13,7 @@ class ProductController():
         self.router.put("/{productId}")(self.update_product)
         self.router.delete("/{productId}")(self.delete_product)
     
-    def create_product(self, product: ProductSchema): 
+    def create_product(self, product: ProductSchema) -> ProductSchema | dict: 
         db = SessionLocal()
         new_product = Product(name=product.name, description=product.description, quantity=product.quantity,price=product.price)
         db.add(new_product)
@@ -22,7 +22,7 @@ class ProductController():
         db.close()
         return new_product
     
-    def get_product(self, product_id: int):
+    def get_product(self, product_id: int) -> ProductSchema | dict:
         db = SessionLocal()
         product = db.query(Product).filter(Product.id == product_id).first()
         db.close()
@@ -30,7 +30,7 @@ class ProductController():
             return {"error": "Product not found."}
         return product 
     
-    def update_product(self, product_id: int, model: ProductSchema):
+    def update_product(self, product_id: int, model: ProductSchema) -> ProductSchema | dict:
         db = SessionLocal()
         product = db.query(Product).filter(Product.id == product_id).first()
         if not product:
@@ -47,7 +47,7 @@ class ProductController():
         db.close()
         return product
     
-    def delete_product(self, product_id: int):
+    def delete_product(self, product_id: int) -> ProductSchema | dict:
         db = SessionLocal()
         product = db.query(Product).filter(Product.id == product_id).first()
         if not product:
